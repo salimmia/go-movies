4f8c29f8debe21c34f8eac52e5b13f1e64310786
selim <salimmia3745@gmail.com>
2023-08-14 21:35:54 +0600

    Returning JSON from our API (43)

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello world from %s", app.Domain)
+	var payload = struct{
+		Status string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status: "active",
+		Message: "Go movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
