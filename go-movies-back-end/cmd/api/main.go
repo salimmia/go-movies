0376ea46babb5362592c2963e653889157d29e5d
selim <salimmia3745@gmail.com>
2023-08-14 20:19:08 +0600

    Added backend

---
+++ go-movies-back-end/cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = 8080
+
+type application struct{
+	Domain string
+}
+
+func main(){
+	// set application config
+	var app application
+
+	app.Domain = "example.com"
+
+	// read from command line
+
+	// connect to database
+
+	// start a web server
+
+	log.Println("Starting application on port", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil{
+		log.Fatal(err)
+		return
+	}
+}
