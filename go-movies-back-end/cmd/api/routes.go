693277f6c477064fa11d6d09a511357dfbcb02e3
selim <salimmia3745@gmail.com>
2023-08-14 20:39:21 +0600

    Installing a routing package (41)

--- go-movies-back-end/cmd/api/routes.go
+++ go-movies-back-end/cmd/api/routes.go
@@ -1,6 +1,16 @@
 package main
 
-func Routes(){
+import (
+	"net/http"
 
-	return
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler{
+	mux := chi.NewRouter()
+	
+	mux.Use(middleware.Recoverer)
+	
+	return mux
 }
