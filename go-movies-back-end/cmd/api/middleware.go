bed0c5c567b0bb9221d70f0ad1a811c6046d808e
selim <salimmia3745@gmail.com>
2023-08-17 22:44:35 +0600

    update enableCORS

--- go-movies-back-end/cmd/api/middleware.go
+++ go-movies-back-end/cmd/api/middleware.go
@@ -7,7 +7,7 @@ func (app *application) enableCORS(h http.Handler) http.Handler{
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		if r.Method == "OPTIONS"{
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Methos", "GET,POST,POST,PATCH,DELETE,OPTIONS")
+			w.Header().Set("Access-Control-Allow-Methos", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept,Content-Type,X-CSRF-Token,Authorization")
 			return
 		}else{
