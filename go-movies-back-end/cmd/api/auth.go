d773398eae446dbb23a8ac966a46dc474d4799b9
selim <salimmia3745@gmail.com>
2023-08-18 19:56:38 +0600

    Generating tokens (59)

--- go-movies-back-end/cmd/api/auth.go
+++ go-movies-back-end/cmd/api/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -30,4 +31,52 @@ type TokenPairs struct{
 
 type Claims struct{
 	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error){
+	// Create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+
+	// Set the expiry for JWT
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// Create a signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil{
+		return TokenPairs{}, err
+	}
+
+
+	// Create a refresh token and set claims
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// Set the expiry for the refresh token
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).UTC()
+
+	// Create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil{
+		return TokenPairs{}, err
+	}
+
+	// Create TokenPairs and populate with signed tokens
+	var tokenPairs = TokenPairs{
+		Token: signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	// return TokenPairs
+	return tokenPairs, nil
 }
