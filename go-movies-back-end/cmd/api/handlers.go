3ffbf50ad0187d44efcb738f2bb7036c15ece546
selim <salimmia3745@gmail.com>
2023-08-18 20:12:00 +0600

    Creating a handler to generate a JWT (61)

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -26,4 +27,29 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.WriteJSON(w, http.StatusOK, movies)
+}
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request){
+	// read json payload
+
+	// validate user against database
+
+	// check password
+
+	// create a jwt user
+	u := jwtUser{
+		ID: 1,
+		FirstName: "Admin",
+		LastName: "User",
+	}
+
+	// generate tokens
+	tokens, err := app.auth.GenerateTokenPair(&u)
+	if err != nil{
+		app.ErrorJSON(w, err)
+		return
+	}
+	log.Println(tokens.Token)
+
+	w.Write([]byte(tokens.Token))
 }
--- go-movies-back-end/cmd/api/routes.go
+++ go-movies-back-end/cmd/api/routes.go
@@ -15,6 +15,8 @@ func (app *application) routes() http.Handler {
 	mux.Use(app.enableCORS)
 
 	mux.Get("/", app.Home)
+	
+	mux.Get("/authenticate", app.authenticate)
 
 	mux.Get("/movies", app.AllMovies)
 
