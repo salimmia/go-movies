45986979ba16c752748a46b3e8e99522e98be701
selim <salimmia3745@gmail.com>
2023-08-18 20:32:52 +0600

    Reading a JSON payload in the authenticate handler (64)

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -31,6 +31,16 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request){
 	// read json payload
+	var requestPayload struct{
+		Email 		string `json:"email"`
+		Password 	string `json:"password"`
+	}
+
+	err := app.ReadJSON(w, r, &requestPayload)
+	if err != nil{
+		app.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
 
 	// validate user against database
 
